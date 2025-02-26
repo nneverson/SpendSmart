9b102020ebd9e30e351d7940b3c505b6771a8b5e
nneverson <neelynev@gmail.com>
2025-02-25 20:18:56 -0500

    create edd and delete funtion.

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -4,42 +4,77 @@ using System.Diagnostics;
 
 namespace SpendSmart.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		private readonly SpendSmartDbContext _context;
+		public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        public IActionResult Expenses()
-        {
-            return View();
-        }
-        public IActionResult CreateEditExpenses()
-        {
-            return View();
-        }
-        public IActionResult SubmitForm(Expense model)
-        {
-            return RedirectToAction("Expenses");
-        }
+		public IActionResult Expenses()
+		{
+			var allExpenses = _context.Expenses.ToList();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			var totalExpenses = allExpenses.Sum(x => x.Value);
+
+			ViewBag.Expenses = totalExpenses;
+
+			return View(allExpenses);
+
+		}
+		public IActionResult CreateEditExpenses(int? id)
+		{
+			if (id != null)
+			{
+				var expenseInDb = _context.Expenses.SingleOrDefault(x => x.Id == id);
+				return View(expenseInDb);
+			}
+			return View();
+
+		}
+		public IActionResult DeleteExpense(int id)
+		{
+			var expenseInDb = _context.Expenses.SingleOrDefault(x => x.Id == id); //take the first expense where the id matches
+			_context.Expenses.Remove(expenseInDb);
+			_context.SaveChanges();
+			return RedirectToAction("Expenses"); //return users to overview page
+		}
+
+		public IActionResult SubmitForm(Expense model)
+
+		{
+			if (model.Id == 0)
+			{
+				_context.Expenses.Add(model);
+
+			}
+			else
+			{
+				_context.Expenses.Update(model);
+			}
+			_context.SaveChanges();
+
+			return RedirectToAction("Expenses");
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
